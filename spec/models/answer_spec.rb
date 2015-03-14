28518478fc08cb44014b1e96e344b615dd24e32b
mr-koww <mr-koww@yandex.ru>
2015-03-14 20:35:02 +0500

    additional answer validate

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -2,5 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
   it { should validate_presence_of :body }
+  it { should validate_length_of(:body).is_at_least(5).is_at_most(250) }
   it { should belong_to :question }
 end
