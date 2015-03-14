0149a908e4822ff9981dd67a41eac82d310b2392
mr-koww <mr-koww@yandex.ru>
2015-03-14 16:51:57 +0500

    additional validates

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,4 +1,6 @@
 class Answer < ActiveRecord::Base
-  validates :body, presence: true
   belongs_to :question
+
+  validates :body, presence: true
+
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,8 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  it { should validate_presence_of :title }
-  it { should validate_presence_of :body }
-  it { should have_many(:answer) }
+  it "1. not nil title" do
+    should validate_presence_of :title
+  end
+
+  it "2. not nil body" do
+    should validate_presence_of :body
+  end
+
+  it "3. title have length in [5..35] range" do
+    should validate_length_of(:title).is_at_least(10).is_at_most(35)
+  end
+
+  it "4. body have length in [10..255] range" do
+    should validate_length_of(:body).is_at_least(20).is_at_most(250)
+  end
+
+  it "5. delete question with all dependent answers" do
+  should have_many(:answer).dependent(:destroy)
+  end
 
 end
