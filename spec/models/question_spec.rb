251f034f49b88630487e7906a5ea133d7c55f314
mr-koww <mr-koww@yandex.ru>
2015-03-15 23:56:57 +0500

    refactoring tests

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,24 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  it "1. not nil title" do
-    should validate_presence_of :title
-  end
 
-  it "2. not nil body" do
-    should validate_presence_of :body
-  end
-
-  it "3. title have length in [5..35] range" do
-    should validate_length_of(:title).is_at_least(10).is_at_most(35)
-  end
-
-  it "4. body have length in [10..255] range" do
-    should validate_length_of(:body).is_at_least(20).is_at_most(250)
-  end
-
-  it "5. delete question with all dependent answers" do
-  should have_many(:answer).dependent(:destroy)
-  end
+  it { should validate_presence_of :title }
+  it { should validate_presence_of :body }
+  it { should validate_length_of(:title).is_at_least(10).is_at_most(35) }
+  it { should validate_length_of(:body).is_at_least(20).is_at_most(250) }
+  it { should have_many(:answer).dependent(:destroy) }
 
 end
