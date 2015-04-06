dfd15d0f5c97ac155c817bfa3b2bc5acdc246d8a
mr-koww <mr-koww@yandex.ru>
2015-04-06 23:00:41 +0500

    refactoring can_delete_answer test

--- spec/acceptance/answer/user_can_delete_answer.rb
+++ spec/acceptance/answer/user_can_delete_answer.rb
@@ -1,6 +1,9 @@
 require_relative '../acceptance_helper'
 
-feature 'User can create delete', type: :feature do
+feature 'User can delete answer', %q{
+  As an author of answer
+  I'd like to be able delete my answer
+} do
   given(:user1) { create(:user) }
   given(:user2) { create(:user) }
   given(:question1) { create(:question, user: user1) }
@@ -8,18 +11,27 @@ feature 'User can create delete', type: :feature do
   given(:question2) { create(:question, user: user1) }
   given!(:answers) { create_list(:answer, 5, question: question2) }
 
-  scenario 'Auth user can delete own answer' do
-    sign_in(user1)
+  describe 'Auth user try delete his answer' do
+    before do
+      sign_in(user1)
+      visit question_path(question1)
+    end
 
-    visit question_path(question1)
-    click_on 'Delete answer'
+    scenario 'see delete link' do
+      expect(page).to have_link 'Delete answer'
+    end
 
-    expect(current_path).to eq question_path(question1)
-    expect(page).to_not have_content answer.body
-    expect(page).to have_content 'Your answer was successfully destroyed.'
+    scenario 'delete his answer', js: true do
+      click_on 'Delete answer'
+
+      expect(current_path).to eq question_path(question1)
+      expect(page).to_not have_content answer.body
+      expect(page).to have_content 'Your answer was successfully destroyed.'
+    end
   end
 
-  scenario 'Auth user cannot delete not own answers' do
+
+  scenario 'Auth user cannot delete not his answer' do
     sign_in(user2)
 
     visit question_path(question1)
@@ -27,7 +39,7 @@ feature 'User can create delete', type: :feature do
     expect(page).to_not have_content 'Delete answer'
   end
 
-  scenario 'cannot delete not own answers' do
+  scenario 'Guest cannot delete any answer' do
     visit question_path(question1)
 
     expect(page).to_not have_content 'Delete answer'
