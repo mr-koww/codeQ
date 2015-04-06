9854059b755d41f67ab10b75ffd461094402d006
mr-koww <mr-koww@yandex.ru>
2015-04-06 16:27:35 +0500

    rename acceptance tests

--- spec/acceptance/answer/user_can_EDIT_answer.rb
+++ spec/acceptance/answer/user_can_EDIT_answer.rb
@@ -1,4 +1,4 @@
-require_relative 'acceptance_helper'
+require_relative '../acceptance_helper'
 
 feature 'User can edit answer', %q{
   In order to fix mistake
--- spec/acceptance/answer/user_can_delete_answer.rb
+++ spec/acceptance/answer/user_can_delete_answer.rb
@@ -1,4 +1,4 @@
-require_relative 'acceptance_helper'
+require_relative '../acceptance_helper'
 
 feature 'User can create delete', type: :feature do
   given(:user1) { create(:user) }
