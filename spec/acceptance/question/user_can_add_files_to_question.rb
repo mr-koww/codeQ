b878276d5d914c00d776e69abd0f3cdb0b2a4288
mr-koww <mr-koww@yandex.ru>
2015-04-13 21:44:05 +0500

    add user_can_add_files_to_question test

---
+++ spec/acceptance/question/user_can_add_files_to_question.rb
@@ -0,0 +1,25 @@
+require_relative '../acceptance_helper'
+
+feature 'User can add files to question', %q{
+  In order to illustrate my question
+  As an author of question
+  I'd like to be able to attach files
+} do
+
+  given(:user) { create(:user) }
+
+  background do
+    sign_in(user)
+    visit new_question_path
+  end
+
+  scenario '' do
+    fill_in 'Title', with: 'My question'
+    fill_in 'Body', with: 'My body text'
+    attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
+    click_on 'Create'
+
+    expect(page).to have_content 'spec_helper.rb'
+
+  end
+end
