4e8f862b55b34e27993b45aa298382a1830ddd61
mr-koww <mr-koww@yandex.ru>
2015-04-14 18:25:38 +0500

    add model Attachment

--- Gemfile
+++ Gemfile
@@ -27,6 +27,8 @@ gem 'slim-rails'
 
 gem 'devise'
 
+gem 'carrierwave'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150413165019) do
+ActiveRecord::Schema.define(version: 20150413165636) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,9 +30,12 @@ ActiveRecord::Schema.define(version: 20150413165019) do
 
   create_table "attachments", force: :cascade do |t|
     t.string   "file"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "question_id"
   end
+
+  add_index "attachments", ["question_id"], name: "index_attachments_on_question_id", using: :btree
 
   create_table "questions", force: :cascade do |t|
     t.string   "title"
