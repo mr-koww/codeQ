dcd765b6ffa4dba88f60d1778a30c884a7d09bac
mr-koww <mr-koww@yandex.ru>
2015-04-17 11:41:51 +0500

    add assigned answer test

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -31,6 +31,10 @@ let(:question) { create :question, user: user1 }
       expect(assigns(:question)).to eq question
     end
 
+    it 'builds new attachment for answer' do
+      expect(assigns(:answer).attachments.first).to be_a_new(Attachment)
+    end
+
     it 'renders show view' do
       expect(response).to render_template :show
     end
