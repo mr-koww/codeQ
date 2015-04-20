153b9393c32fbb9577d8c6fd2c9d1ba89d3f58fb
mr-koww <mr-koww@yandex.ru>
2015-04-20 16:10:38 +0500

    refactoring tests

--- spec/acceptance/answer/user_can_create_answer_spec.rb
+++ spec/acceptance/answer/user_can_create_answer_spec.rb
@@ -1,31 +1,46 @@
 require_relative '../acceptance_helper'
 
-feature 'User can create answer', type: :feature do
-  given(:user) { create(:user) }
-  given(:question) { create(:question, user: user) }
-  given(:answer) { create(:answer, question: question, user: user) }
+feature 'User can create answer', %q{
+  In order to answer the question
+  As an authenticated user
+  I'd like to be able create answer
+} do
+  given(:user_question) { create(:user) }
+  given(:user_answer) { create(:user) }
 
-  before { sign_in(user) }
+  given(:question) { create(:question, user: user_question) }
 
-  scenario 'with valid data', js: true do
-    visit question_path(question)
+  describe 'Auth user try' do
+    before { sign_in(user_answer) }
 
-    fill_in 'Your answer', with: answer.body
-    click_on 'Add answer'
+    scenario 'create answer with valid data', js: true do
+      visit question_path(question)
 
-    expect(current_path).to eq question_path(question)
-    within '.answers' do
-      expect(page).to have_content answer.body
+      fill_in 'answer[body]', with: "My answer"
+      click_on I18n.t('answer.button.create')
+
+      within '.answers' do
+        expect(page).to have_content "My answer"
+      end
+      expect(page).to have_content I18n.t('answer.notice.create.success')
+      expect(current_path).to eq question_path(question)
+    end
+
+    scenario 'with invalid data', js: true do
+      visit question_path(question)
+
+      click_on I18n.t('answer.button.create')
+
+      expect(current_path).to eq question_path(question)
+      expect(page).to have_content I18n.t('answer.notice.create.fail')
     end
   end
 
-  scenario 'with invalid data', js: true do
-    visit question_path(question)
+  describe 'Not auth user try' do
+    scenario 'create answer' do
+      visit question_path(question)
 
-    click_on 'Add answer'
-
-    expect(current_path).to eq question_path(question)
-    expect(page).to have_content "Body is too short"
+      expect(page).to_not have_content I18n.t('answer.button.new')
+    end
   end
-
 end
