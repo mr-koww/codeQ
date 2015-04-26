fdd553f456f2f93e5a1efe5bf3c2a33678304c09
mr-koww <mr-koww@yandex.ru>
2015-04-26 11:28:55 +0500

    define bootstrap alert class method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+
+  def bootstrap_alert_class(flash_type)
+    case flash_type
+      when 'success'
+        "alert-success"
+      when 'error'
+        "alert-error"
+      when 'alert'
+        "alert-danger"
+      when 'notice'
+        "alert-info"
+      else
+        flash_type.to_s
+    end
+  end
+
 end
