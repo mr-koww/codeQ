38fbc3853a70f663060e81dccc495b380fb34d01
mr-koww <mr-koww@yandex.ru>
2015-04-26 18:33:41 +0500

    add user presence validate

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Question, type: :model do
 
   it { should validate_presence_of :title }
   it { should validate_presence_of :body }
+  it { should validate_presence_of :user }
   it { should validate_length_of(:title).is_at_least(10).is_at_most(35) }
   it { should validate_length_of(:body).is_at_least(10).is_at_most(250) }
 
