67d08b33b608e0dd04706e336284fe8803bdf69c
mr-koww <mr-koww@yandex.ru>
2015-04-28 15:25:19 +0500

    vote model

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,7 +1,22 @@
 class Vote < ActiveRecord::Base
   belongs_to :user
+  belongs_to :votable, polymorphic: true
 
   validates :value, :user, :votable_id, :votable_type, presence: true
   validates :value, inclusion: [1, -1]
   validates :votable_type, inclusion: ['Question', 'Answer']
+
+
+  #def voted_by?(user)
+  #  votes.where(user: user).any?
+  #end
+
+  def total_votes
+    votes.sum(:value)
+  end
+
+  #def vote_by(user)
+  #  vote = votes.find_by(user: user)
+  #  vote.value unless vote.nil?
+  #end
 end
