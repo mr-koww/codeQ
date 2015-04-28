ab5c9638f0625e53ebf910e2d4b1efa51001828d
mr-koww <mr-koww@yandex.ru>
2015-04-28 15:25:41 +0500

    votable concern

--- app/models/concerns/votable.rb
+++ app/models/concerns/votable.rb
@@ -5,4 +5,17 @@ module Votable
     has_many :votes, as: :votable, dependent: :destroy
   end
 
+  def vote(user, value)
+    vote = votes.find_or_initialize_by(user: user)
+    vote.value = value
+    vote.save!
+  end
+
+  def unvote(user)
+    votes.where(user: user).delete_all
+  end
+
+  def total_votes
+    votes.sum(:value)
+  end
 end
