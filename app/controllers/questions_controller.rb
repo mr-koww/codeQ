cff8558a1cf3d7c265a88307fd64f219e6e6db96
mr-koww <mr-koww@yandex.ru>
2015-05-06 11:40:40 +0500

    refactoring controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,60 +1,57 @@
 class QuestionsController < ApplicationController
-
   before_action :authenticate_user!, except: [ :index, :show ]
-  before_action :load_question, only: [ :show, :edit, :update, :destroy, :like, :dislike, :unvote ]
-
+  before_action :load_question, except: [ :index, :new, :create ]
+  before_action :question_author?, only: [ :update, :destroy ]
+  before_action :build_answer, only: [ :show ]
+  after_action  :publish_question, only: [ :create ]
   include Voted
 
-  def index
-    @questions = Question.all
-  end
+  respond_to :js
 
+  def index
+    respond_with(@questions = Question.all)
+  end
 
   def show
-    @answers = @question.answers
-    @answer = Answer.new
+    respond_with @question
   end
-
 
   def new
-    @question = Question.new
+    respond_with(@question = Question.new)
   end
-
 
   def create
-    @question = Question.new(question_params)
-    @question.user = current_user
-    if @question.save
-      redirect_to @question, notice: t('question.notice.create.success')
-    else
-      flash[:notice] = t('question.notice.create.fail')
-      render :new
-    end
+    respond_with(@question = Question.create(question_params))
   end
-
 
   def update
     @question.update(question_params)
+    respond_with @question
   end
-
 
   def destroy
-    if current_user.id == @question.user_id
-      @question.destroy!
-      redirect_to questions_path, notice: t('question.notice.delete.success')
-    else
-      redirect_to @question
-    end
+    respond_with(@question.destroy)
   end
-
 
   private
   def load_question
     @question = Question.find(params[:id])
   end
 
-  def question_params
-    params.require(:question).permit(:title, :body, attachments_attributes: [:id, :file, :_destroy])
+  def question_author?
+    render nothing: true, status: :forbidden unless current_user.id == @question.user_id
   end
 
+  def build_answer
+    @answer = @question.answers.build
+  end
+
+  def question_params
+    params.require(:question).permit(:title, :body, attachments_attributes: [:id, :file, :_destroy])
+      .merge(user_id: current_user.id)
+  end
+
+  def publish_question
+    PrivatePub.publish_to "/questions", question: @question.to_json
+  end
 end
