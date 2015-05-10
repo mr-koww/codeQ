7582748682d5b2c94724e970075e6ed53abff745
mr-koww <mr-koww@yandex.ru>
2015-05-10 13:19:23 +0500

    add github keys

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -232,7 +232,8 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-   config.omniauth :facebook, Rails.application.secrets.facebook_app_id, Rails.application.secrets.facebook_app_secret, scope: [:email]
+  config.omniauth :facebook, Rails.application.secrets.facebook_app_id, Rails.application.secrets.facebook_app_secret, scope: [:email]
+  config.omniauth :github, Rails.application.secrets.github_app_id, Rails.application.secrets.github_app_secret, scope: [:user]
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
