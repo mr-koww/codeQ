c975fa3d5e65853af0e9d011a421674e60cd7b34
mr-koww <mr-koww@yandex.ru>
2015-05-10 13:17:57 +0500

    add letter_opener

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,18 +1,22 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
-    @user = User.find_for_oauth(request.env['omniauth.auth'])
-    if @user.persisted?
-      sign_in_and_redirect @user, event: :authentication
-      set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
-    end
+    sign_in_oauth('Facebook')
   end
 
   def github
+    sign_in_oauth('Github')
+  end
+
+  private
+
+  def sign_in_oauth(provider)
     @user = User.find_for_oauth(request.env['omniauth.auth'])
     if @user.persisted?
       sign_in_and_redirect @user, event: :authentication
-      set_flash_message(:notice, :success, kind: 'Github') if is_navigational_format?
+      set_flash_message(:notice, :success, kind: provider) if is_navigational_format?
+    else
+      flash[:alert] = 'Something went wrong. Try a different login method.'
+      redirect_to new_user_session_path
     end
   end
-
 end
