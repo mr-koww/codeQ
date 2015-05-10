dcf1afa7370181528bd593ac3e06933318371bfb
mr-koww <mr-koww@yandex.ru>
2015-05-10 10:31:53 +0500

    add github

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -7,4 +7,12 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     end
   end
 
+  def github
+    @user = User.find_for_oauth(request.env['omniauth.auth'])
+    if @user.persisted?
+      sign_in_and_redirect @user, event: :authentication
+      set_flash_message(:notice, :success, kind: 'Github') if is_navigational_format?
+    end
+  end
+
 end
