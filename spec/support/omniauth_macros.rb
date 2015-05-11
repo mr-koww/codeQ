a3cd79d5410497fa7658265c39b7fd6b8ec67044
mr-koww <mr-koww@yandex.ru>
2015-05-11 10:46:58 +0500

    oauth macros

--- spec/support/omniauth_macros.rb
+++ spec/support/omniauth_macros.rb
@@ -2,11 +2,22 @@ module OmniauthMacros
   def facebook_mock
     OmniAuth.config.mock_auth[:facebook] = OmniAuth::AuthHash.new({
         provider: 'facebook',
-        uid: '123545',
+        uid: '12345',
         info: {
           email: 'email@email.ru'
         }
     })
   end
+
+  def github_mock
+    OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new({
+        provider: 'github',
+        uid: '12345',
+        info: {
+          email: ''
+        }
+    })
+  end
+
 
 end
