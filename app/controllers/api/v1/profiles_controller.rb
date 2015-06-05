523a6e5f46260099a64aa4918598303938e293f8
mr-koww <mr-koww@yandex.ru>
2015-06-05 13:26:46 +0500

    refactoring profiles controller

---
+++ app/controllers/api/v1/profiles_controller.rb
@@ -0,0 +1,11 @@
+class Api::V1::ProfilesController < Api::V1::BaseController
+
+  def index
+    users = User.where.not(id: current_resource_owner.id)
+    respond_with users
+  end
+
+  def me
+    respond_with current_resource_owner
+  end
+end
