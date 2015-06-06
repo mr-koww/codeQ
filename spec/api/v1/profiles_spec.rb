8fca60a2b39b0ebd92d2b8e66f5cc9fc5a6b90b9
mr-koww <mr-koww@yandex.ru>
2015-06-06 14:09:35 +0500

    add tests

--- spec/api/v1/profiles_spec.rb
+++ spec/api/v1/profiles_spec.rb
@@ -39,34 +39,49 @@ describe 'Profile API' do
   end
 
   describe 'GET /profiles' do
-    let(:me) { create(:user) }
-    let!(:users) { create_list(:user, 2) }
-    let(:access_token) { create(:access_token, resource_owner_id: me.id) }
+    context 'unauthorized' do
+      it 'returns 401 status if there is no access_token' do
+        get '/api/v1/profiles', format: :json
+        expect(response.status).to eq 401
+      end
 
-    before { get '/api/v1/profiles', format: :json, access_token: access_token.token }
-
-    it 'returns 200' do
-      expect(response.status).to eq 200
-    end
-
-    %w(id email).each do |attr|
-      it "contains #{attr}" do
-        users.each_with_index do |user, i|
-          expect(response.body).to be_json_eql(user.send(attr.to_sym).to_json).at_path("#{i}/#{attr}")
-        end
+      it 'returns 401 status if access_token is invalid' do
+        get '/api/v1/profiles', format: :json, access_token: '12345'
+        expect(response.status).to eq 401
       end
     end
 
-    %w(password encrypted_password).each do |attr|
-      it "contains #{attr}" do
-        users.each_with_index do |user, i|
-          expect(response.body).to_not have_json_path("#{i}/#{attr}")
+    context 'authorized' do
+      let(:me) { create(:user) }
+      let!(:users) { create_list(:user, 2) }
+      let(:access_token) { create(:access_token, resource_owner_id: me.id) }
+
+      before { get '/api/v1/profiles', format: :json, access_token: access_token.token }
+
+      it 'returns 200' do
+        expect(response.status).to eq 200
+      end
+
+      %w(id email).each do |attr|
+        it "contains #{attr}" do
+          users.each_with_index do |user, i|
+            expect(response.body).to be_json_eql(user.send(attr.to_sym).to_json).at_path("#{i}/#{attr}")
+          end
         end
+      end
+
+      %w(password encrypted_password).each do |attr|
+        it "contains #{attr}" do
+          users.each_with_index do |user, i|
+            expect(response.body).to_not have_json_path("#{i}/#{attr}")
+          end
+        end
+      end
+
+      it 'does not return current user' do
+        expect(response.body).to_not include_json(me.to_json)
       end
     end
 
-    it 'does not return current user' do
-      expect(response.body).to_not include_json(me.to_json)
-    end
   end
 end
