61aca8371b1a9dc0d04240bac3f3016f9c75e54b
mr-koww <mr-koww@yandex.ru>
2015-06-13 17:11:08 +0500

    add confirm user

--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -1,7 +1,9 @@
 module ControllerMacros
   def sign_in_user(user)
-      @user = user
-      @request.env['devise.mapping'] = Devise.mappings[:user]
-      sign_in @user
+    @user = user
+    @user.confirmed_at = Time.now
+    @user.save
+    @request.env['devise.mapping'] = Devise.mappings[:user]
+    sign_in @user
   end
 end
