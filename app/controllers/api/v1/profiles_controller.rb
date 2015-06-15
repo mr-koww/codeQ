17bb5f52aab4f3c8f76c91b7d9a33dd1c699be2f
mr-koww <mr-koww@yandex.ru>
2015-06-15 14:34:38 +0500

    refactoring

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -2,8 +2,8 @@ class Api::V1::ProfilesController < Api::V1::BaseController
   authorize_resource :user
 
   def index
-    users = User.where.not(id: current_resource_owner.id)
-    respond_with users
+    @users = User.where.not(id: current_resource_owner.id)
+    respond_with @users
   end
 
   def me
