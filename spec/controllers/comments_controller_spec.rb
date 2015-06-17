6ae02571303797b8bfd3eb7dab82227e4b9a4b3d
mr-koww <mr-koww@yandex.ru>
2015-06-17 13:15:50 +0500

    refactoring tests

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -3,167 +3,121 @@ require 'rails_helper'
 describe CommentsController, type: :controller do
   let(:author) { create(:user) }
   let(:user) { create(:user) }
-  let(:question) { create(:question, user: author) }
-  let(:answer) { create(:answer, user: author, question: question) }
 
-  describe 'POST #create' do
-    context 'when auth user tries comment QUESTION' do
+  let(:commentable) { create(:question, user: author) }
+  let(:commentable_type) { 'questions' }
+  let(:commentable_param) {  }
+
+  describe 'POST /create' do
+    def request(attributes = {})
+      post(:create, commentable: commentable_type, question_id: commentable, comment: attributes_for(:comment), format: :js)
+    end
+
+    it_behaves_like 'unauthorized response for not auth user'
+
+    context 'auth user' do
+      before { sign_in_user(author) }
+
       context 'with valid attributes' do
-        let(:create_question_comment) do
-          post(:create, commentable: 'questions', question_id: question, comment: attributes_for(:comment), format: :json)
-        end
-
-        before { sign_in_user(user) }
-
-        it 'saves a new comment in the database' do
-          expect { create_question_comment }.to change(Comment, :count).by(1)
+        it 'saves new comment in database' do
+          expect { request }.to change(Comment, :count).by(1)
         end
 
         it 'associates the new comment with the question' do
-          expect { create_question_comment }.to change(question.comments, :count).by(1)
+          expect { request }.to change(commentable.comments, :count).by(1)
+        end
+
+        it 'should have the user' do
+          request
+          expect(assigns(:comment).user).to eq subject.current_user
         end
       end
 
       context 'with invalid attributes' do
-        let(:create_question_comment) do
-          post(:create, commentable: 'questions', question_id: question, comment: { body: nil }, format: :json)
+        def bad_request
+          post :create, commentable: commentable_type, question_id: commentable, comment: { body: nil }, format: :js
         end
-
         before { sign_in_user(user) }
 
         it 'does not save a new comment in the database' do
-          expect { create_question_comment }.to_not change(Comment, :count)
-        end
-
-        it 'has Unprocessable Entity response status' do
-          create_question_comment
-          expect(response).to have_http_status(422)
-        end
-      end
-
-      context 'Non authenticated user' do
-        it 'does not save a comment in the database' do
-          expect { post(:create, commentable: 'questions', question_id: question,
-              comment: { body: nil }, format: :json) }.to_not change(Comment, :count)
-        end
-      end
-    end
-
-    context 'when auth user tries comment ANSWER' do
-      context 'with valid attributes' do
-        let(:create_answer_comment) do
-          post(:create, commentable: 'answers', answer_id: answer, comment: attributes_for(:comment), format: :json)
-        end
-
-        before { sign_in_user(user) }
-
-        it 'saves a new comment in the database' do
-          expect { create_answer_comment }.to change(Comment, :count).by(1)
-        end
-
-        it 'associates the new comment with the answer' do
-          expect { create_answer_comment }.to change(answer.comments, :count).by(1)
-        end
-      end
-
-      context 'with invalid attributes' do
-        let(:create_answer_comment) do
-          post(:create, commentable: 'answers', answer_id: answer, comment: { body: nil }, format: :json)
-        end
-
-        before { sign_in_user(user) }
-
-        it 'does not save a new comment in the database' do
-          expect { create_answer_comment }.to_not change(Comment, :count)
-        end
-
-        it 'has Unprocessable Entity response status' do
-          create_answer_comment
-          expect(response).to have_http_status(422)
-        end
-      end
-
-      context 'Non authenticated user' do
-        it 'does not save a comment in the database' do
-          expect { post(:create, commentable: 'answers', answer_id: answer,
-              comment: { body: nil }, format: :json) }.to_not change(Comment, :count)
+          expect { bad_request }.to_not change(Comment, :count)
         end
       end
     end
   end
 
-
-  describe 'PATCH #update' do
-    let!(:comment) { create(:comment, commentable: question, user: author) }
-
-    let(:update_question_comment) do
-      patch(:update, commentable: 'questions', id: comment,
-          comment: { body: 'New comment' }, format: :json)
+  describe 'PATCH /update' do
+    let!(:comment) { create(:comment, commentable: commentable, user: author) }
+    def request(attributes = {})
+      patch(:update, commentable: commentable_type, id: comment, comment: { body: 'New comment' }, format: :js)
     end
 
-    context 'when auth user tries update own comment' do
-      before { sign_in_user(author) }
+    it_behaves_like 'unauthorized response for not auth user'
 
-      context 'with valid attributes' do
-        it 'changes comment attributes' do
-          update_question_comment
-          comment.reload
-          expect(comment.body).to eq 'New comment'
+    context 'auth user' do
+      context 'own comment' do
+        before { sign_in_user(author) }
+
+        context 'with valid attributes' do
+          it 'changes comment attributes' do
+            request
+            comment.reload
+            expect(comment.body).to eq 'New comment'
+          end
+        end
+
+        context 'with invalid attributes' do
+          def bad_request(attributes = {})
+            patch(:update, commentable: commentable_type, id: comment, comment: { body: nil }, format: :js)
+          end
+
+          it 'doesnt changes the comment in  database' do
+            old_comment_body = comment.body
+            bad_request
+            comment.reload
+            expect(comment.body).to eq old_comment_body
+          end
         end
       end
 
-      context 'with invalid attributes' do
-        let(:update_question_comment) do
-          patch(:update, commentable: 'questions', question_id: question, id: comment,
-              comment: { body: nil }, format: :json)
-        end
+      context 'not own comment' do
+        before { sign_in_user(user) }
 
-        it 'does not change the comment in the database' do
+        it_behaves_like 'forbidden response'
+
+        it 'doesnt changes the comment in  database' do
           old_comment_body = comment.body
-          update_question_comment
+          request
           comment.reload
           expect(comment.body).to eq old_comment_body
         end
-
-        it 'has Unprocessable Entity response status' do
-          update_question_comment
-          expect(response).to have_http_status(422)
-        end
-      end
-    end
-
-    context 'when auth user tries update not own comment' do
-      before { sign_in_user(user) }
-
-      it 'has Forbidden response status' do
-        update_question_comment
-        expect(response).to have_http_status(403)
       end
     end
   end
 
-
-  describe 'DESTROY #delete' do
-    let!(:comment) { create(:comment, commentable: question, user: author) }
-
-    let(:destroy_question_comment) do
-      patch(:destroy, commentable: 'questions', id: comment, format: :json)
+  describe 'DELETE /destroy' do
+    let!(:comment) { create(:comment, commentable: commentable, user: author) }
+    def request(attributes = {})
+      patch(:destroy, commentable: commentable_type, id: comment, format: :js)
     end
 
-    context 'when auth user tries destroy own comment' do
-      before { sign_in_user(author) }
+    it_behaves_like 'unauthorized response for not auth user'
 
-      it 'deletes comment from the database' do
-        expect { destroy_question_comment }.to change(Comment, :count).by(-1)
+    context 'auth user' do
+      context 'own comment' do
+        before { sign_in_user(author) }
+
+        it 'deletes comment from the database' do
+          expect { request }.to change(Comment, :count).by(-1)
+        end
       end
-    end
 
-    context 'when auth user tries destroy not own comment' do
-      before { sign_in_user(user) }
+      context 'not own comment' do
+        before { sign_in_user(user) }
 
-      it 'has Forbidden response status' do
-        destroy_question_comment
-        expect(response).to have_http_status(403)
+        it 'doesnt deletes comment from the database' do
+          expect { request }.to_not change(Comment, :count)
+        end
       end
     end
   end
