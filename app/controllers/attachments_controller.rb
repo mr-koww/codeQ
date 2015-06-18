ef558231b6c2707f47726c8c9543508157d98a5c
mr-koww <mr-koww@yandex.ru>
2015-06-18 09:13:15 +0500

    delete destroy method

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -1,23 +1,18 @@
 class AttachmentsController < ApplicationController
   before_action :authenticate_user!
   before_action :load_attachment
-  before_action :author_attachment?
 
-  respond_to :js, :json
+  respond_to :js
 
   authorize_resource
 
-  def destroy
-    respond_with(@attachment.destroy)
-  end
+  # def destroy
+  #   respond_with(@attachment.destroy)
+  # end
 
   private
 
   def load_attachment
     @attachment = Attachment.find(params[:id])
-  end
-
-  def author_attachment?
-    render nothing: true, status: :forbidden unless @attachment.attachable.user_id == current_user.id
   end
 end
