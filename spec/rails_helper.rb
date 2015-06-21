5dc3b7e8d60b8f3f0413610df1a1ef46034eb483
mr-koww <mr-koww@yandex.ru>
2015-06-21 11:16:22 +0500

    add sidekiq testing

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,6 +3,9 @@ require 'spec_helper'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
 require 'cancan/matchers'
+require 'sidekiq/testing'
+
+Sidekiq::Testing.inline!
 
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
