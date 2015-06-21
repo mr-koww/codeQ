d2eb1a51384b60104242f32a9cc07fcfbddfe98c
mr-koww <mr-koww@yandex.ru>
2015-06-21 20:32:21 +0500

    add abilities for Subscriber

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -22,11 +22,16 @@ class Ability
 
   def user_abilities
     guest_abilities
-    can :create, [ Question, Answer, Comment ]
+    can :create, [ Question, Answer, Comment, Subscriber ]
     can :update, [ Question, Answer, Comment ], user: user
     can :destroy, [ Question, Answer, Comment ], user: user
+
     can :destroy, Attachment do |attachment|
       attachment.attachable.user_id == user.id
+    end
+
+    can :destroy, Subscriber do |subscriber|
+      user.subscribers.where(question_id: subscriber.question).present?
     end
 
     can :best, Answer do |answer|
