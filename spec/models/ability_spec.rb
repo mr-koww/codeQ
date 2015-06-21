dba3f720285eee50d9d2505f9bc72d38f10b32c4
mr-koww <mr-koww@yandex.ru>
2015-06-21 20:32:38 +0500

    add tests

--- spec/models/ability_spec.rb
+++ spec/models/ability_spec.rb
@@ -14,7 +14,7 @@ describe Ability, type: :model do
   end
 
   describe 'for admin' do
-    let(:user) { create(:user, admin:true) }
+    let(:user) { create(:user, admin: true) }
 
     it { should be_able_to :manage, :all}
   end
@@ -22,14 +22,15 @@ describe Ability, type: :model do
   describe 'for auth user' do
     let(:user) { create(:user) }
     let(:other) { create(:user) }
-    let(:my_question) { create(:question, user: user) }
+
+    let(:question) { create(:question, user: user) }
     let(:other_question) { create(:question, user: other) }
 
-    let(:my_answer) { create(:answer, question: my_question, user: user) }
-    let(:other_answer) { create(:answer, question: my_question, user: other) }
+    let(:answer) { create(:answer, question: question, user: user) }
+    let(:other_answer) { create(:answer, question: question, user: other) }
 
-    let(:my_comment) { create(:comment, commentable: my_question, user: user) }
-    let(:other_comment) { create(:comment, commentable: my_question, user: other) }
+    let(:comment) { create(:comment, commentable: question, user: user) }
+    let(:other_comment) { create(:comment, commentable: question, user: other) }
 
     it { should_not be_able_to :manage, :all }
     it { should be_able_to :read, :all }
@@ -40,45 +41,66 @@ describe Ability, type: :model do
     it { should be_able_to :create, Comment }
 
     # update
-    it { should be_able_to :update, my_question, user: user }
+    it { should be_able_to :update, question, user: user }
     it { should_not be_able_to :update, other_question, user: user }
 
-    it { should be_able_to :update, my_answer, user: user }
+    it { should be_able_to :update, answer, user: user }
     it { should_not be_able_to :update, other_answer, user: user }
 
-    it { should be_able_to :update, my_comment, user: user }
+    it { should be_able_to :update, comment, user: user }
     it { should_not be_able_to :update, other_comment, user: user }
 
     # destroy
-    it { should be_able_to :destroy, my_question, user: user }
+    it { should be_able_to :destroy, question, user: user }
     it { should_not be_able_to :destroy, other_question, user: user }
 
-    it { should be_able_to :destroy, my_answer, user: user }
+    it { should be_able_to :destroy, answer, user: user }
     it { should_not be_able_to :destroy, other_answer, user: user }
 
-    it { should be_able_to :destroy, my_comment, user: user }
+    it { should be_able_to :destroy, comment, user: user }
     it { should_not be_able_to :destroy, other_comment, user: user }
 
     # like/dislike/unvote (Question)
-    it { should_not be_able_to :like, my_question, user: user }
+    it { should_not be_able_to :like, question, user: user }
     it { should be_able_to :like, other_question, user: user }
 
-    it { should_not be_able_to :dislike, my_question, user: user }
+    it { should_not be_able_to :dislike, question, user: user }
     it { should be_able_to :dislike, other_question, user: user }
 
-    it { should_not be_able_to :unvote, my_question, user: user }
+    it { should_not be_able_to :unvote, question, user: user }
 
     # like/dislike/unvote (Answer)
-    it { should_not be_able_to :like, my_answer, user: user }
+    it { should_not be_able_to :like, answer, user: user }
     it { should be_able_to :like, other_answer, user: user }
 
-    it { should_not be_able_to :dislike, my_answer, user: user }
+    it { should_not be_able_to :dislike, answer, user: user }
     it { should be_able_to :dislike, other_answer, user: user }
 
-    it { should_not be_able_to :unvote, my_answer, user: user }
+    it { should_not be_able_to :unvote, answer, user: user }
 
     # best (Answer)
-    it { should_not be_able_to :best, my_answer, user: user }
+    it { should_not be_able_to :best, answer, user: user }
     it { should be_able_to :best, other_answer, user: user }
+
+
+    describe 'Subscribe' do
+      context ':create' do
+        it { should be_able_to :create, Subscriber }
+        end
+
+      context ':destroy' do
+        context 'when subscribed' do
+          let(:subscriber) { create(:subscriber, question: question, user: user) }
+
+          it { should be_able_to :destroy, subscriber }
+        end
+
+        context 'when not subscribed (other subscribe)' do
+          let(:subscriber) { create(:subscriber, question: question, user: other) }
+
+          it { should_not be_able_to :destroy, subscriber }
+        end
+      end
+    end
   end
 end
