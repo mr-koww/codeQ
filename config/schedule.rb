c364c8dee82a07640451fac914ad5433253b0856
mr-koww <mr-koww@yandex.ru>
2015-06-21 11:22:05 +0500

    add schedule

---
+++ config/schedule.rb
@@ -0,0 +1,18 @@
+# Example:
+#
+# set :output, "/path/to/my/cron_log.log"
+#
+# every 2.hours do
+#   command "/usr/bin/some_great_command"
+#   runner "MyModel.some_method"
+#   rake "some:great:rake:task"
+# end
+#
+# every 4.days do
+#   runner "AnotherModel.prune_old_records"
+# end
+
+every 1.day, at: '9:30 am' do
+  runner "DailyDigestJob.create"
+end
+
