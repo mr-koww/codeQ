7c71d32ca3921cccf65223a7f496713d00dd39fe
mr-koww <mr-koww@yandex.ru>
2015-06-23 11:51:38 +0500

    add location for respond_to

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -3,16 +3,17 @@ class SubscribersController < ApplicationController
   before_action :load_question, only: [ :create ]
   before_action :load_subscriber, only: [ :destroy ]
 
-  respond_to :js
+  #respond_to :js
 
   authorize_resource
 
   def create
-    respond_with @question.subscribers.find_or_create_by(user: current_user)
+    respond_with @question.subscribers.find_or_create_by(user: current_user), location: @question
   end
 
   def destroy
-    respond_with @subscriber.destroy
+    @question = @subscriber.question
+    respond_with(@subscriber.destroy, location: @question)
   end
 
   private
