077f3dd00bf27d13e16e3c0028b5e427a11d6443
mr-koww <mr-koww@yandex.ru>
2015-06-26 11:10:04 +0500

    add search route

--- config/routes.rb
+++ config/routes.rb
@@ -11,10 +11,7 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
 
-  resources :users, only: [ :show, :update ] do
-    patch :change_email, on: :member
-  end
-
+  # Concerns:
   concern :votable do
     member do
       patch :like
@@ -23,10 +20,26 @@ Rails.application.routes.draw do
     end
   end
 
+  # API:
+  namespace :api do
+    namespace :v1 do
+      resources :profiles, only: :index do
+        get :me, on: :collection
+      end
+      resources :questions, only: [ :index, :show, :create ] do
+        resources :answers, only: [ :index, :show, :create ], shallow: true
+      end
+    end
+  end
+
+  resources :users, only: [ :show, :update ] do
+    patch :change_email, on: :member
+  end
+
   resources :questions, concerns: [ :votable ] do
     resources :answers, only: [ :create, :update, :destroy ], concerns: [ :votable ], shallow: true do
       put :best, on: :member
-      resources :comments, only: :create, defaults: { commentable: 'answer' }
+    resources :comments, only: :create, defaults: { commentable: 'answer' }
     end
 
     resources :comments, only: :create, defaults: { commentable: 'question' }
@@ -36,15 +49,5 @@ Rails.application.routes.draw do
   resources :attachments, only: [ :destroy ]
   resources :comments, only: [ :update, :destroy ], shallow: true
 
-  namespace :api do
-    namespace :v1 do
-      resources :profiles, only: :index do
-        get :me, on: :collection
-      end
-      resources :questions, only: [ :index, :show, :create ] do
-        resources :answers, only: [ :index, :show, :create ], shallow: true
-      end
-
-    end
-  end
+  get '/search', to: 'search#index', as: :search
 end
