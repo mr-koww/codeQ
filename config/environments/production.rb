d7b65b19274ef5cdec4c28e4c379b061e1f53f46
mr-koww <mr-koww@yandex.ru>
2015-06-29 09:36:23 +0500

    fix hostname

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,5 +77,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: '85.143.216.241' }
+  config.action_mailer.default_url_options = { host: '27607.simplecloud.club' }
 end
