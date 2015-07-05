06d4a030d0b60fe2c2fe9ed6df6a6f2b6095febc
mr-koww <mr-koww@yandex.ru>
2015-07-05 15:19:01 +0500

    add redis_store for cache_store

--- config/application.rb
+++ config/application.rb
@@ -38,5 +38,7 @@ module CodeQ
                         controller_spec: true
        g.fixture_replacement :factory_girl, dir: 'spec/factories'
     end
+
+    config.cache_store = :redis_store, 'redis://localhost:6379/0/cache', { expires_in: 90.minutes }
   end
 end
