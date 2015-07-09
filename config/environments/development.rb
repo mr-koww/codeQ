81c420b505732f869933c0b7c259c888d5f9047a
mr-koww <mr-koww@yandex.ru>
2015-07-09 10:43:35 +0500

    automate load from lib (comment)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,6 +9,8 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
+  # ActiveSupport::Dependencies.autoload_paths << File::join( Rails.root, 'lib')
+
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = true
